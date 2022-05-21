9db38b3da1078f76ecd9d4dd2e89906d1b5f5a46
Matthew Xu <xuwanjin1991@gmail.com>
2022-05-21 20:04:10 +0800

    first commit
    Signed-off-by: xuwanjin <xuwj@mxchip.com>

---
+++ cue/cue/cueApp.swift
@@ -0,0 +1,17 @@
+//
+//  cueApp.swift
+//  cue
+//
+//  Created by Matthew on 2022/5/21.
+//
+
+import SwiftUI
+
+@main
+struct cueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
