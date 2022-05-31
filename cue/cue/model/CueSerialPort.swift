f62c8d9792f239e944cc14d037a4d415a2b82553
Matthew Xu <xuwanjin1991@gmail.com>
2022-05-31 09:13:32 +0800

    修改默认值
    Signed-off-by: xuwanjin <xuwj@mxchip.com>

--- cue/cue/model/CueSerialPort.swift
+++ cue/cue/model/CueSerialPort.swift
@@ -10,7 +10,7 @@ import Foundation
 struct CueSerialPort: Hashable{
 
     var portName: String = PortNameConstants.PortNameList[0]
-    var baudRate: String = BaudrateConstants.BaudRate_List[4]
+    var baudRate: String = BaudrateConstants.BaudRate_List[0]
     var dataBits: String = DataBitsConstants.DataBits_List[2]
     var stopBits: String = StopBitsConstants.StopBits_List[1]
     var parity: String = ParityConstants.Parity_List[0]
